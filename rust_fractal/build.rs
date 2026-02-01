8e63904346954fd1232ffaabf11dcc06783e1955
virtualnonsense <virtualnonse@mailbox.org>
2026-02-01 17:04:40 +0100

    rust lib boilerplate

---
+++ rust_fractal/build.rs
@@ -0,0 +1,16 @@
+use std::{error::Error, fs, path::PathBuf, str::FromStr};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let package_name = env!("CARGO_PKG_NAME");
+    let build_dir = PathBuf::from_str(env!("CARGO_MANIFEST_DIR"))?;
+    let csharp_dir = build_dir.join("target").join("csharp");
+    let file_name = format!("{package_name}.g.cs");
+
+    fs::create_dir_all(&csharp_dir)?;
+
+    csbindgen::Builder::default()
+        .input_extern_file("./src/lib.rs")
+        .csharp_dll_name(package_name)
+        .csharp_class_accessibility("internal")
+        .generate_csharp_file(csharp_dir.join(file_name))
+}
