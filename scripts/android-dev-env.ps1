4024cadacc8a8144d358ab2cd3e118ed781f86b6
virtualnonsense <virtualnonse@mailbox.org>
2026-02-02 21:34:42 +0100

    fixed script

--- scripts/android-dev-env.ps1
+++ scripts/android-dev-env.ps1
@@ -1,7 +1,7 @@
 
 # Must be dot-sourced so env changes persist in the caller
-if ($MyInvocation.InvocationName -ne '.') {
-    throw "Dot-source this script: `. .\scripts\android-dev-env.ps1`"
+if ($MyInvocation.InvocationName -ne ".") {
+    throw "Dot-source this script: . .\scripts\android-dev-env.ps1"
 }
 
 $env:ANDROID_NDK_HOME = "$env:LOCALAPPDATA\Android\Ndk\r27d"
