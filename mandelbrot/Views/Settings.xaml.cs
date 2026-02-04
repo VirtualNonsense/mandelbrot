43aba5235a4491a3fe524eb8a8df0edd07a8c8f9
virtualnonsense <virtualnonse@mailbox.org>
2026-02-05 00:15:06 +0100

    Added simple renderer selection

--- mandelbrot/Views/Settings.xaml.cs
+++ mandelbrot/Views/Settings.xaml.cs
@@ -1,26 +1,55 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using mandelbrot.Render;
+using mandelbrot.Services;
 
 namespace mandelbrot.Views;
 
+public sealed record EnumOption<TEnum>(TEnum Value, string Label)
+    where TEnum : struct, Enum
+{
+    public override string ToString()
+    {
+        return Label;
+    }
+};
+
 public partial class Settings : ContentPage
 {
-    public Settings()
+    public ObservableCollection<EnumOption<Approach>> Approaches { get; } =
+        new(Enum.GetValues<Approach>().Select(v => new EnumOption<Approach>(v, ToLabel(v))));
+
+    private static string ToLabel(Approach approach) => approach switch
     {
+        Approach.CSharpBaseLine => "CsharpBaseline",
+        Approach.NaiveRustRenderer => "RustRenderer",
+        _ => "unknown"
+    };
+
+    public EnumOption<Approach> SelectedApproach
+    {
+        get => new(_state.SelectedApproach, ToLabel(_state.SelectedApproach));
+        set => _state.SelectedApproach = value.Value;
+    }
+
+
+    private AppState _state;
+
+    public Settings(AppState state)
+    {
+        _state = state;
         InitializeComponent();
+        BindingContext = this;
         if (Application.Current is App app)
         {
             app.UserAppTheme = app.PlatformAppTheme;
         }
     }
 
+
     private void OnToggleTheme(object? sender, EventArgs e)
     {
         if (Application.Current is not App app) return;
         var theme = app.UserAppTheme;
-        app.UserAppTheme = theme == AppTheme.Light? AppTheme.Dark : AppTheme.Light;
+        app.UserAppTheme = theme == AppTheme.Light ? AppTheme.Dark : AppTheme.Light;
     }
 }
